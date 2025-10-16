7c19da2875578fab5380256ee855f0361e556707
HanChendi <77253366+ChendiHan111@users.noreply.github.com>
2025-10-16 13:34:57 +0800

    Update multi-class-prediction2.py

--- multi-class-prediction2.py
+++ multi-class-prediction2.py
@@ -468,7 +468,7 @@ if __name__=='__main__':
 
     filename = ['Multi-Class-data2\Multi-Class Dataset.csv',
                      'input-features\drug initial embedding representations.csv','input-features\Morgan fingerprint vectors.csv',
-                    'input-features\drug subgraph mean representations.csv','input-features\drug subgraph frequency representations.csv']
+                    'input-features\drug subgraph mean representations.csv','input-features\drug subgraph frequency representations.csv','input-features\extra label vectors.csv']
     
 
     data_list, drugs = get_drugpair_info(filename[0], [], [])
